1d0c0afd3cff151b28f5f17ff800c341996620c1
BrianvanWinden <78685297+BrianvanWinden@users.noreply.github.com>
2021-03-22 17:00:35 +0100

    Update load_data.py

--- ecg/load_data.py
+++ ecg/load_data.py
@@ -1,8 +1,21 @@
 import pandas as pd
 import os
+from zipfile import ZipFile
 
 
 def load_data():
-    this_directory = os.path.dirname(os.path.abspath(__file__))
+    this_directory = os.path.dirname(os.path.abspath(_file_))
+    zip_dir = os.path.join(this_directory, 'ecg_data.zip')
+    zf = ZipFile(zip_dir, "r")
+    zf.extractall(this_directory)
     data = pd.read_csv(os.path.join(this_directory, 'ecg_data.csv'), index_col=0)
-    return data
+    return  data
+
+
+
+
+
+
+
+
+
